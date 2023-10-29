3f08c1d67af276233b701d0f6d33b2ae7d5e90e3
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-10-29 18:32:26 +0100

    Fix animation channel box

--- src/AnimationPlayer/ChannelBox.cs
+++ src/AnimationPlayer/ChannelBox.cs
@@ -2,15 +2,15 @@
 {
     public class ChannelBox : BinarySerializable
     {
-        public sbyte MinY { get; set; }
         public sbyte MaxY { get; set; }
+        public sbyte MinY { get; set; }
         public sbyte MinX { get; set; }
         public sbyte MaxX { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            MinY = s.Serialize<sbyte>(MinY, name: nameof(MinY));
             MaxY = s.Serialize<sbyte>(MaxY, name: nameof(MaxY));
+            MinY = s.Serialize<sbyte>(MinY, name: nameof(MinY));
             MinX = s.Serialize<sbyte>(MinX, name: nameof(MinX));
             MaxX = s.Serialize<sbyte>(MaxX, name: nameof(MaxX));
         }
