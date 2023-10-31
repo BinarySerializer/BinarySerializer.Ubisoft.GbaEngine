b822751d83ee78ac394b647c3f3f1d91d0860e0d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-10-31 22:51:26 +0100

    Use FixedPointInt16 for AffineMatrix

--- src/AnimationPlayer/AffineMatrix.cs
+++ src/AnimationPlayer/AffineMatrix.cs
@@ -2,17 +2,17 @@
 {
     public class AffineMatrix : BinarySerializable, ISerializerShortLog
     {
-        public short Pa { get; set; }
-        public short Pb { get; set; }
-        public short Pc { get; set; }
-        public short Pd { get; set; }
+        public FixedPointInt16 Pa { get; set; }
+        public FixedPointInt16 Pb { get; set; }
+        public FixedPointInt16 Pc { get; set; }
+        public FixedPointInt16 Pd { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Pa = s.Serialize<short>(Pa, name: nameof(Pa));
-            Pb = s.Serialize<short>(Pb, name: nameof(Pb));
-            Pc = s.Serialize<short>(Pc, name: nameof(Pc));
-            Pd = s.Serialize<short>(Pd, name: nameof(Pd));
+            Pa = s.SerializeObject<FixedPointInt16>(Pa, x => x.Pre_PointPosition = 8, name: nameof(Pa));
+            Pb = s.SerializeObject<FixedPointInt16>(Pb, x => x.Pre_PointPosition = 8, name: nameof(Pb));
+            Pc = s.SerializeObject<FixedPointInt16>(Pc, x => x.Pre_PointPosition = 8, name: nameof(Pc));
+            Pd = s.SerializeObject<FixedPointInt16>(Pd, x => x.Pre_PointPosition = 8, name: nameof(Pd));
         }
 
         public string ShortLog => ToString();
