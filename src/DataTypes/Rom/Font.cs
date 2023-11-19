b7b7d89058bd968df38ab3a9b205761e59232f19
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-11-19 12:25:53 +0100

    Serialize font

---
+++ src/DataTypes/Rom/Font.cs
@@ -0,0 +1,26 @@
+﻿namespace BinarySerializer.Onyx.Gba
+{
+    public class Font : BinarySerializable
+    {
+        public const int CharactersCount = 256;
+
+        public ushort CharacterHeight { get; set; }
+        public ushort ImgDataSize { get; set; }
+
+        public byte[] CharacterWidths { get; set; }
+        public ushort[] CharacterOffsets { get; set; }
+
+        public byte[] ImgData { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            CharacterHeight = s.Serialize<ushort>(CharacterHeight, name: nameof(CharacterHeight));
+            ImgDataSize = s.Serialize<ushort>(ImgDataSize, name: nameof(ImgDataSize));
+
+            CharacterWidths = s.SerializeArray<byte>(CharacterWidths, CharactersCount, name: nameof(CharacterWidths));
+            CharacterOffsets = s.SerializeArray<ushort>(CharacterOffsets, CharactersCount, name: nameof(CharacterOffsets));
+
+            ImgData = s.SerializeArray<byte>(ImgData, ImgDataSize, name: nameof(ImgData));
+        }
+    }
+}
