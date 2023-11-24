7f8828a4829a91941305e668af1184ba503f5b18
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-11-24 22:38:53 +0100

    Allow loader to load GBA ROM header

--- src/Loader/GbaLoader.cs
+++ src/Loader/GbaLoader.cs
@@ -6,9 +6,16 @@ namespace BinarySerializer.Onyx.Gba
     {
         public GbaLoader(Context context) : base(context) { }
 
+        public ROMHeader RomHeader { get; protected set; }
+
         public void LoadFiles(string romFileName, bool cache)
         {
             LoadFile(romFileName, Constants.Address_ROM, cache);
+        }
+
+        public void LoadRomHeader(string romFileName)
+        {
+            RomHeader = FileFactory.Read<ROMHeader>(Context, romFileName);
         }
 
         public void LoadData(string romFileName)
