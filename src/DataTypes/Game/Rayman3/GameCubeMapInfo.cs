b8e70f8f3740e5dde966dc27b75c321a500207a4
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-02-03 09:29:47 +0100

    Serialize GameCube map info

---
+++ src/DataTypes/Game/Rayman3/GameCubeMapInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace BinarySerializer.Ubisoft.GbaEngine.Rayman3
+{
+    public class GameCubeMapInfo : BinarySerializable
+    {
+        public byte CagesCount { get; set; }
+        public byte LumsCount { get; set; }
+
+        public Rayman3SoundEvent StartMusicSoundEvent { get; set; }
+        public Rayman3SoundEvent StopMusicSoundEvent { get; set; }
+
+        public bool HasBlueLum { get; set; }
+
+        public string Name { get; set; }
+        public int FileSize { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            CagesCount = s.Serialize<byte>(CagesCount, name: nameof(CagesCount));
+            LumsCount = s.Serialize<byte>(LumsCount, name: nameof(LumsCount));
+
+            StartMusicSoundEvent = s.Serialize<Rayman3SoundEvent>(StartMusicSoundEvent, name: nameof(StartMusicSoundEvent));
+            StopMusicSoundEvent = s.Serialize<Rayman3SoundEvent>(StopMusicSoundEvent, name: nameof(StopMusicSoundEvent));
+
+            HasBlueLum = s.Serialize<bool>(HasBlueLum, name: nameof(HasBlueLum));
+            s.SerializePadding(1, logIfNotNull: true);
+
+            Name = s.SerializeString(Name, 32, name: nameof(Name));
+            FileSize = s.Serialize<int>(FileSize, name: nameof(FileSize));
+        }
+    }
+}
---
+++ src/DataTypes/Game/Rayman3/GameCubeMapInfos.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySerializer.Ubisoft.GbaEngine.Rayman3
+{
+    public class GameCubeMapInfos : BinarySerializable
+    {
+        public int MapsCount { get; set; }
+        public GameCubeMapInfo[] Maps { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            MapsCount = s.Serialize<int>(MapsCount, name: nameof(MapsCount));
+            Maps = s.SerializeObjectArray<GameCubeMapInfo>(Maps, MapsCount, name: nameof(Maps));
+        }
+    }
+}
