ca24697fd6eb81bff9404c6b9542246d94429841
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-02-15 19:33:06 +0100

    Change actor links to be nullable

--- src/DataTypes/Scene2D/Actor.cs
+++ src/DataTypes/Scene2D/Actor.cs
@@ -5,7 +5,7 @@
         public byte Type { get; set; }
         public byte Idx_ActorModel { get; set; }
         public byte FirstActionId { get; set; }
-        public byte[] Links { get; set; }
+        public byte?[] Links { get; set; }
 
         // Dependencies
         public ActorModel Model { get; set; }
@@ -17,7 +17,7 @@
             Type = s.Serialize<byte>(Type, name: nameof(Type));
             Idx_ActorModel = s.Serialize<byte>(Idx_ActorModel, name: nameof(Idx_ActorModel));
             FirstActionId = s.Serialize<byte>(FirstActionId, name: nameof(FirstActionId));
-            Links = s.SerializeArray<byte>(Links, 4, name: nameof(Links));
+            Links = s.SerializeNullableArray<byte>(Links, 4, name: nameof(Links));
         }
     }
 }
