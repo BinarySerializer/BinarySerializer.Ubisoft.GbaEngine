a753db445eb186dbace88be4630120521b72f512
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-08-09 13:26:42 +0200

    Add RawResource class

---
+++ src/DataTypes/GbaCommon/GbaSdk/Sdk/RawResource.cs
@@ -0,0 +1,12 @@
+﻿namespace BinarySerializer.Ubisoft.GbaEngine
+{
+    public class RawResource : Resource
+    {
+        public byte[] RawData { get; set; }
+
+        public override void SerializeResource(SerializerObject s)
+        {
+            RawData = s.SerializeArray<byte>(RawData, Size, name: nameof(RawData));
+        }
+    }
+}
