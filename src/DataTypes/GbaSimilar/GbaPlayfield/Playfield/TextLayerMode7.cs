009b1bc20ba0a1d3d32fb0d1421076fbc3b6b304
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-02-03 21:45:59 +0100

    Fill out TextLayerMode7

--- src/DataTypes/GbaSimilar/GbaPlayfield/Playfield/TextLayerMode7.cs
+++ src/DataTypes/GbaSimilar/GbaPlayfield/Playfield/TextLayerMode7.cs
@@ -6,15 +6,13 @@
         public bool HasAlphaBlending { get; set; }
         public byte AlphaCoeff { get; set; }
         public byte BackgroundSize { get; set; }
-        public int Int_0C { get; set; }
-        public short Short_10 { get; set; }
-        public short Short_12 { get; set; }
+        public FixedPointInt32 RotationFactor { get; set; }
+        public Vector2 Vector2_10 { get; set; } // Unused in Rayman 3
         public Vector2 MapDimensions { get; set; }
-        public byte[] Bytes_18 { get; set; } // Unused in Rayman 3
-        public short Short_1A { get; set; }
+        public Vector2 BasePosition { get; set; } // X value is unused in Rayman 3
         public byte MapBlock { get; set; }
         public byte Priority { get; set; }
-        public byte Byte_1E { get; set; }
+        public byte Byte_1E { get; set; } // Unused in Rayman 3 - always the same value as LayerId
         public bool Is8Bit { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
@@ -23,12 +21,10 @@
             HasAlphaBlending = s.Serialize<bool>(HasAlphaBlending, name: nameof(HasAlphaBlending));
             AlphaCoeff = s.Serialize<byte>(AlphaCoeff, name: nameof(AlphaCoeff));
             BackgroundSize = s.Serialize<byte>(BackgroundSize, name: nameof(BackgroundSize));
-            Int_0C = s.Serialize<int>(Int_0C, name: nameof(Int_0C));
-            Short_10 = s.Serialize<short>(Short_10, name: nameof(Short_10));
-            Short_12 = s.Serialize<short>(Short_12, name: nameof(Short_12));
+            RotationFactor = s.SerializeObject<FixedPointInt32>(RotationFactor, x => x.Pre_PointPosition = 16, name: nameof(RotationFactor));
+            Vector2_10 = s.SerializeObject<Vector2>(Vector2_10, name: nameof(Vector2_10));
             MapDimensions = s.SerializeObject<Vector2>(MapDimensions, name: nameof(MapDimensions));
-            Bytes_18 = s.SerializeArray<byte>(Bytes_18, 2, name: nameof(Bytes_18));
-            Short_1A = s.Serialize<short>(Short_1A, name: nameof(Short_1A));
+            BasePosition = s.SerializeObject<Vector2>(BasePosition, name: nameof(BasePosition));
             MapBlock = s.Serialize<byte>(MapBlock, name: nameof(MapBlock));
             Priority = s.Serialize<byte>(Priority, name: nameof(Priority));
             Byte_1E = s.Serialize<byte>(Byte_1E, name: nameof(Byte_1E));
