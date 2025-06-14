e358359a53f66cc9671bc315c44aa0eb9c9636fd
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-06-14 20:35:07 +0200

    Allow getting resource type from setting

--- src/Settings/GbaEngineSettings.cs
+++ src/Settings/GbaEngineSettings.cs
@@ -11,20 +11,36 @@ namespace BinarySerializer.Ubisoft.GbaEngine
 
         public OffsetTable RootTable { get; set; }
         public Dictionary<Enum, int> DefinedResources { get; set; }
+        public Dictionary<int, Type> DefinedResourceTypes { get; set; }
 
         public void SetDefinedResources<T>(Dictionary<int, DefinedResourceInfo<T>> definedResources)
             where T : Enum
         {
             DefinedResources = definedResources.ToDictionary(x => (Enum)x.Value.Resource, x => x.Key);
+            DefinedResourceTypes = definedResources.ToDictionary(x => x.Key, x => x.Value.Type);
         }
 
         public int GetDefinedResourceId<T>(T definedResource)
             where T : Enum
         {
-            if (DefinedResources == null)
+            if (DefinedResources == null || DefinedResourceTypes == null)
                 throw new Exception("The resources have not been defined");
 
             return DefinedResources[definedResource];
+        }
+
+        public Type GetDefinedResourceType(int resourceId)
+        {
+            if (DefinedResources == null || DefinedResourceTypes == null)
+                throw new Exception("The resources have not been defined");
+
+            return DefinedResourceTypes.TryGetValue(resourceId, out Type type) ? type : typeof(Scene2D);
+        }
+
+        public Type GetDefinedResourceType<T>(T definedResource)
+            where T : Enum
+        {
+            return GetDefinedResourceType(GetDefinedResourceId(definedResource));
         }
     }
 }
