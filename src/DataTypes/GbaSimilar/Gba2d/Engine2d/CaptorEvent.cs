f6dd5772f51d222c5a8319daa3065939ff3aec7f
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-05-09 21:32:04 +0200

    Rename Captor.TriggersCount to Captor.TriggerIterationIndex

--- src/DataTypes/GbaSimilar/Gba2d/Engine2d/CaptorEvent.cs
+++ src/DataTypes/GbaSimilar/Gba2d/Engine2d/CaptorEvent.cs
@@ -4,13 +4,13 @@
     {
         public ushort MessageId { get; set; }
         public ushort Param { get; set; }
-        public short TriggersCount { get; set; }
+        public short TriggerIterationIndex { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
             MessageId = s.Serialize<ushort>(MessageId, name: nameof(MessageId));
             Param = s.Serialize<ushort>(Param, name: nameof(Param));
-            TriggersCount = s.Serialize<short>(TriggersCount, name: nameof(TriggersCount));
+            TriggerIterationIndex = s.Serialize<short>(TriggerIterationIndex, name: nameof(TriggerIterationIndex));
         }
     }
 }
